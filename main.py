726bc761e29e4c302b6a8b3add5ce1144a9f2ad3
Gigi Khmaladze <46771671+gkhmalad@users.noreply.github.com>
2021-06-12 17:45:05 +0400

    Testcode

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from bs4 import BeautifulSoup
 url = "https://www.twitch.tv/trainwreckstv/clips?filter=clips&range=24hr"
 
 def get_video_links():
+    #Response Object
     r = requests.get(url)
+
+    #BS object
     soup = BeautifulSoup(r.content, 'html5lib')
+
+    #Find all links
     links = soup.findAll('a')
+
+    #Filter the link ending with .mp43
+    
